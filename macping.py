41c98359c37c68f3c74cd4f373e847dc89495c9f
Kota Yatagai <kota@yatagai.com>
2025-01-21 17:17:13 +0900

    virtio logging, wondering illegal_instruction

--- macping.py
+++
@@ -1,4 +0,0 @@
-from scapy.all import Ether, sendp
-
-packet = Ether(dst="52:54:00:12:34:56") / "Hello"
-sendp(packet, iface="qemubr0")
--- user.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "common.h"
-
-__attribute__((noreturn)) void exit(void);
-void putchar(char ch);
-int getchar(void);
